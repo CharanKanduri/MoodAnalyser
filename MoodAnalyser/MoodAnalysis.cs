aabc6c8557d6783b248cf52120fbafe5e9803fae
charan_kanduri <charan2000kanduri@gmail.com>
2021-07-24 00:23:38 +0530

    [CHARAN]UC-1 Guess MOod

---
+++ MoodAnalyser/MoodAnalysis.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    public class MoodAnalyser
+    {
+        string[] message;
+        //Parameterised Constructor
+        public MoodAnalyser(string[] message)
+        {
+            this.message = message;
+        }
+
+        public string ReturnMessage()
+        {
+            if (message.Contains("sad"))
+            {
+                return "Sad";
+            }
+            else
+            {
+                return "Happy";
+            }
+        }
+    }
+}
---
+++ MoodAnalyser/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MoodAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get input statement from user
+            Console.WriteLine("Welcome To Mood Analyser!");
+            string[] message = Console.ReadLine().ToLower().Split(" ");
+            MoodAnalyser setmood = new MoodAnalyser(message);
+            Console.WriteLine("Mood is: {0}", setmood.ReturnMessage());
+        }
+    }
+}
